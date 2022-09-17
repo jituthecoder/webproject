f09af693e78526e85fb78f651d81c70b7643f64d
Jitendra <jituthecoder@gmail.com>
2022-09-17 22:55:55 +0530

    After adding the start script in package.json file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const port = process.env.PORT || 8000;
+
 const app = express();
 
 app.get('/',(req,res)=>{
     res.send('This is after the compliting ssh key.');
 });
 
-app.listen('8000');
+app.listen(port);
